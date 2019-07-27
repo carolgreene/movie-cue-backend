1dad5140832d549d737205e90d6966e9c58b5a3a
carolgreene <carolgreene3@gmail.com>
2019-07-27 14:53:55 -0700

    wrote basic controller methods for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,51 @@
 class MoviesController < ApplicationController
+
+  #GET /movies
+  def index 
+    @movies = Movie.all  
+
+    render json: @movies
+  end
+
+  #GET /movies/1
+  def show 
+    render json @movie
+  end
+
+  #POST /movies
+  def create 
+    @movie = Movie.new(movie_params)
+
+    if @movie.save 
+      render json: @movie, status: :created, location: @movie 
+    else 
+      render json: @movie.errors, status: :unprocessable_entity
+    end 
+  end
+
+  #PATCH/PUT movies/1
+  def update 
+    if @movie.update(movie_params)
+      render json: @movie
+    else
+      render json: @movie.errors, status: unprocessable_entity
+    end 
+  end
+
+  #DELETE /movies/1
+  def destroy 
+    @movie.destroy 
+  end
+
+  private 
+    #use callbacks to share common setup or constraints
+    def set_movie 
+      @movie = Movie.find(params[:id])
+    end
+
+    #Only allow trusted parameter "white list" through
+    def movie_params 
+      params.require(:movie).permit(:title, :genre, :rating, :description, :runtime)
+    end
+
 end
