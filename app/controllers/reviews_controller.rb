0e3b3b3464f25bde5f06ec4fbcbbeaf44f420e8b
carolgreene <carolgreene3@gmail.com>
2019-07-27 15:02:28 -0700

    wrote basic controller methods for Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,51 @@
 class ReviewsController < ApplicationController
+
+   #GET /reviews
+   def index 
+    @reviews = Review.all  
+
+    render json: @reviews
+  end
+
+  #GET /reviews/1
+  def show 
+    render json @review
+  end
+
+  #POST /reviews
+  def create 
+    @review = Review.new(review_params)
+
+    if @review.save 
+      render json: @review, status: :created, location: @review
+    else 
+      render json: @review.errors, status: :unprocessable_entity
+    end 
+  end
+
+  #PATCH/PUT reviews/1
+  def update 
+    if @review.update(review_params)
+      render json: @review
+    else
+      render json: @review.errors, status: unprocessable_entity
+    end 
+  end
+
+  #DELETE /reviews/1
+  def destroy 
+    @review.destroy 
+  end
+
+  private 
+    #use callbacks to share common setup or constraints
+    def set_review 
+      @review = Review.find(params[:id])
+    end
+
+    #Only allow trusted parameter "white list" through
+    def review_params 
+      params.require(:review).permit(:movie_id, :rating, :comment)
+    end
+  
 end
