a002741ceec510116364047286a4808a0afd4d88
carolgreene <carolgreene3@gmail.com>
2019-08-06 19:06:18 -0700

    added routes to nest reviews in movies

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       resources :reviews
-      resources :movies
+      resources :movies do
+        resources :reviews            #nested reviews inside of movies
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
