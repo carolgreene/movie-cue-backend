bc6f8268498e54b91de94ea8acdf164926960d46
carolgreene <carolgreene3@gmail.com>
2019-08-06 17:41:09 -0700

    added comment

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -2,7 +2,7 @@ class MovieSerializer
   include FastJsonapi::ObjectSerializer 
   attributes :title, :genre, :rating, :description, :runtime
 
-  has_many :reviews, serializer: ReviewSerializer
+  has_many :reviews, serializer: ReviewSerializer  #not sure if I need the serializer part
 
   attribute :reviews do |movie|
     movie.reviews.map do |review|
